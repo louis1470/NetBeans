789d76b8db1bdcc16d69498c6e108ff8eff758d1
Louis Payne <louisziggypayne@gmail.com>
2023-12-01 18:29:49 +0100

    1/12/23-18:29

--- T6ActStr5/src/t6actstr5/T6ActStr5.java
+++ T6ActStr5/src/t6actstr5/T6ActStr5.java
@@ -20,39 +20,16 @@ public class T6ActStr5 {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         String frase;
-        int c1 = 0, c2 = 0;
         System.out.print("Dame una frase: ");
-        frase = entrada.nextLine();
+        frase = entrada.nextLine().replaceAll(" ", "");
         char f1[] = frase.toLowerCase().toCharArray();
         System.out.println(Arrays.toString(f1));
-        for (int i = 0; i < frase.length(); i++) {
-            
-            switch (f1[i]) {
-                case ' ':
-                    break;
-                default:
-                    c1++;
-
-            }
-        }
-        char f2[] = new char[c1];
-        for (int j = 0; j < frase.length(); j++) {
-            switch (f1[j]) {
-                case ' ':
-                    c2++;
-                    break;
-                default:
-                    f2[j - c2] = f1[j];
-
-            }
+        char f2[] = new char[f1.length];
+        for (int i = 0; i < f1.length; i++) {
+            f2[f2.length - 1 - i] = f1[i];
         }
         System.out.println(Arrays.toString(f2));
-        char f3[] = new char[f2.length];
-        for (int i = 0; i < f2.length; i++) {
-            f3[f3.length - 1 - i] = f2[i];
-        }
-        System.out.println(Arrays.toString(f3));
-        if(Arrays.equals(f2, f3) == true){
+        if (Arrays.equals(f1, f2)) {
             System.out.println("Es palindromo");
         } else {
             System.out.println("No es palindromo");
